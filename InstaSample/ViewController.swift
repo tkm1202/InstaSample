5db78ae9b3a350b477f141ff8ef167ae8b832100
tkm1202 <tkm1202@users.noreply.github.com>
2020-05-23 23:40:01 +0900

    Initial Commit

---
+++ InstaSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaSample
+//
+//  Created by 加藤拓洋 on 2020/05/23.
+//  Copyright © 2020 TakumiKato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
